da37e243c00e40d6201b039ccf210c566e4c390f
FontanR <r.fontan12@gmail.com>
2023-05-28 00:14:29 -0300

    MR-45: Create modal and add put method

--- src/Components/Members/index.js
+++ src/Components/Members/index.js
@@ -1,25 +1,42 @@
 import { useEffect, useState } from 'react';
 import styles from './members.module.css';
+import Table from '../Table';
+import AddMemberModal from '../Form';
 
 function Members() {
   const [members, setMembers] = useState([]);
 
   useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/users`)
+    fetch(`http://localhost:4000/api/member/`)
       .then((response) => response.json())
       .then((response) => {
-        setMembers(response);
+        setMembers(response.data);
       });
   }, []);
+
+  const addMember = async (member) => {
+    // const newMeber = { ...member };
+    // setMembers([...members, newMeber]);
+    const res = await fetch(`http://localhost:4000/api/member/`, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(member)
+    });
+    const data = await res.json();
+
+    setMembers([...members, data]);
+  };
+
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <section className={styles.container}>
       <h2>Members</h2>
-      <div>
-        {members.map((employee) => {
-          return <div key={employee.id}>{employee.name}</div>;
-        })}
-      </div>
+      <button onClick={() => setIsOpen(true)}>Add Member</button>
+      <AddMemberModal open={isOpen} onClose={() => setIsOpen(false)} addMember={addMember} />
+      <Table data={members} />
     </section>
   );
 }
