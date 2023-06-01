1d1866ba517e9f818d3b39770611d89da5cd1594
FontanR <r.fontan12@gmail.com>
2023-06-01 00:11:54 -0300

    MR-45: Add some corrections

--- src/Components/Members/index.jsx
+++ src/Components/Members/index.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './members.module.css';
 import Table from './Table';
 import MemberModal from './Form';
