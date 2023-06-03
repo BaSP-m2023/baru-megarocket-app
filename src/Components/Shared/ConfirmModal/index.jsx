93eeefa0ebadd620375eb4a808ba991d76c3902b
francoax <francoa.duarte2001@gmail.com>
2023-06-03 16:51:19 -0300

    MR-60: component funcionallity done.

--- src/Components/Shared/ConfirmModal/index.jsx
+++ src/Components/Shared/ConfirmModal/index.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import styles from './modalConfirm.module.css';
 
-const ConfirmModal = ({ children, show, handleModal, title, onAction }) => {
-  if (!show) return null;
+const ConfirmModal = ({ children, handleModal, title, onAction }) => {
   return ReactDOM.createPortal(
     <div className={styles.modal}>
       <div className={styles.modalContent}>
