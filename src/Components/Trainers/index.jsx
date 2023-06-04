ed5777a9887ec1e0680468ed3b3b05407720a7a0
Mauro Caffesse <mscaffesse@gmail.com>
2023-06-04 19:43:30 -0300

    MR-80: Layout and Trainers.jsx modified due to the routing

--- src/Components/Trainers/index.jsx
+++ src/Components/Trainers/index.jsx
@@ -1,15 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import styles from './trainers.module.css';
 import Table from './Table';
 import Button from '../Shared/Button';
-import Form from './Form';
-import Modal from './Modals/Modal';
 import ResponseModal from './Modals/ResponseModal';
-import TrainerRoutes from './routes';
 
 const Trainers = () => {
-  const [showModal, setShowModal] = useState(false);
   const [activeTrainers, setActiveTrainers] = useState([]);
   const [showResponseModal, setShowResponseModal] = useState(false);
   const [responseMessage, setResponseMessage] = useState('');
@@ -28,22 +24,6 @@ const Trainers = () => {
     getTrainers();
   }, []);
 
-  /*   const getTrainer = async (id) => {
-    try {
-      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/trainer/${id}`);
-      if (response.ok) {
-        const { data } = await response.json();
-        return data;
-      } else {
-        setResponseMessage('Error getting trainer');
-        setShowResponseModal(true);
-      }
-    } catch (error) {
-      setResponseMessage(`Error fetching trainer: ${error.message}`);
-      setShowResponseModal(true);
-    }
-  }; */
-
   const deleteTrainer = async (id) => {
     try {
       await fetch(`${process.env.REACT_APP_API_URL}/api/trainer/${id}`, {
@@ -56,77 +36,21 @@ const Trainers = () => {
     }
   };
 
-  const addTrainer = async (trainer) => {
-    try {
-      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/trainer`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(trainer)
-      });
-      if (response.ok) {
-        setResponseMessage('Trainer added successfully');
-        setShowResponseModal(true);
-        setShowModal(false);
-        getTrainers();
-      } else {
-        setResponseMessage('Failed to add trainer');
-        setShowResponseModal(true);
-      }
-    } catch (error) {
-      setResponseMessage(`Error adding trainer: ${error.message}`);
-      setShowResponseModal(true);
-    }
-  };
-
-  const updTrainer = async (id, updatedTrainer) => {
-    try {
-      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/trainer/${id}`, {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(updatedTrainer)
-      });
-      if (response.ok) {
-        setResponseMessage('Trainer updated successfully');
-        setShowResponseModal(true);
-        setShowModal(false);
-        getTrainers();
-      } else {
-        setResponseMessage('Failed to update trainer');
-        setShowResponseModal(true);
-      }
-    } catch (error) {
-      setResponseMessage(`Error updating trainer: ${error.message}`);
-      setShowResponseModal(true);
-    }
-  };
-
   return (
-    <Router>
-      <section className={styles.container}>
-        <h2>Trainers</h2>
-        {activeTrainers.length > 0 ? (
-          <Table data={activeTrainers} deleteTrainer={deleteTrainer} updTrainer={updTrainer} />
-        ) : (
-          'There is no trainers to show'
-        )}
-        <TrainerRoutes />
-        <Link to="/trainers/add">
-          <Button action={() => setShowModal(true)} text="+ Add New" classNameButton="addButton" />
-        </Link>
-        {showModal && (
-          <Modal title="Add trainer" onClose={() => setShowModal(false)}>
-            <Form add={addTrainer} />
-          </Modal>
-        )}
-        {showResponseModal && (
-          <ResponseModal text={responseMessage} onClose={() => setShowResponseModal(false)} />
-        )}
-      </section>
-    </Router>
+    <section className={styles.container}>
+      <h2>Trainers</h2>
+      {activeTrainers.length > 0 ? (
+        <Table data={activeTrainers} deleteTrainer={deleteTrainer} />
+      ) : (
+        'There is no trainers to show'
+      )}
+      <Link to="/trainers/add">
+        <Button text="+ Add New" classNameButton="addButton" />
+      </Link>
+      {showResponseModal && (
+        <ResponseModal text={responseMessage} onClose={() => setShowResponseModal(false)} />
+      )}
+    </section>
   );
 };
 
