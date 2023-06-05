9fe66ebe1566c37f3940a61eb0ec3dc1b42324ae
josedaniele <josedaniele493@gmail.com>
2023-06-05 11:51:41 -0300

    MR-82: Form Styles Shared

--- src/Components/Classes/ClassForm/index.jsx
+++ src/Components/Classes/ClassForm/index.jsx
@@ -64,7 +64,7 @@ function ClassForm() {
     !isCreateRoute && getById(id);
   }, []);
   useEffect(() => {
-    if (showModal) {
+    if (showModal && !responseModal.error) {
       const timer = setTimeout(() => {
         history.goBack();
       }, 2000);
@@ -167,114 +167,106 @@ function ClassForm() {
   };
 
   return (
-    <>
-      <div className={styles.formContainer}>
-        {activities.length !== 0 && trainers.length !== 0 ? (
-          <form>
-            <div className={styles.subContainer}>
-              <div className={styles.inputContainer}>
-                <label className={styles.label}>Activity</label>
-                <select
-                  value={classes.activity}
-                  name="activity"
-                  className={styles.select}
-                  onChange={onChangeInput}
-                >
-                  <option value="">Select an activity</option>
-                  {activities.map((activity) => (
-                    <option key={activity._id} value={activity._id}>
-                      {activity.name}
-                    </option>
-                  ))}
-                </select>
-                {error && classes.activity === '' ? (
-                  <span className={styles.error}>Field is required</span>
-                ) : null}
-              </div>
-              <div className={styles.inputContainer}>
-                <label className={styles.label}>Trainer</label>
-                <select
-                  className={styles.select}
-                  value={classes.trainer}
-                  name="trainer"
-                  onChange={onChangeInput}
-                >
-                  <option value="">Select a trainer</option>
-                  {trainers.map((trainer) => (
-                    <option key={trainer._id} value={trainer._id}>
-                      {trainer.firstName}
-                    </option>
-                  ))}
-                </select>
-                {error && classes.trainer === '' ? (
-                  <span className={styles.error}>Field is required</span>
-                ) : null}
-              </div>
-              <div className={styles.inputContainer}>
-                <label className={styles.label}>Day</label>
-                <select
-                  className={styles.select}
-                  value={classes.day}
-                  name="day"
-                  onChange={onChangeInput}
-                >
-                  <option value="">Select a day</option>
-                  <option value="Monday">Monday</option>
-                  <option value="Tuesday">Tuesday</option>
-                  <option value="Wednesday">Wednesday</option>
-                  <option value="Thursday">Thursday</option>
-                  <option value="Friday">Friday</option>
-                  <option value="Saturday">Saturday</option>
-                  <option value="Sunday">Sunday</option>
-                </select>
-                {error && classes.day === '' ? (
-                  <span className={styles.error}>Field is required</span>
-                ) : null}
-              </div>
-              <div className={styles.inputContainer}>
-                <label className={styles.label}>Time</label>
-                <input
-                  className={styles.input}
-                  type="time"
-                  value={classes.time}
-                  name="time"
-                  onChange={onChangeInput}
-                />
-                {(error && classes.time === '') || (error && classes.time === 0) ? (
-                  <span className={styles.error}>Field is required</span>
-                ) : null}
-              </div>
-              <div className={styles.inputContainer}>
-                <label className={styles.label}>Capacity</label>
-                <input
-                  className={styles.input}
-                  type="number"
-                  value={classes.capacity}
-                  name="capacity"
-                  onChange={onChangeInput}
-                />
-                {error && classes.capacity === '' ? (
-                  <span className={styles.error}>Field is required</span>
-                ) : null}
-              </div>
-              <div className={styles.buttonContainer}>
-                <button
-                  className={styles.button}
-                  onClick={isCreateRoute ? onClickCreateClass : onClickEditClass}
-                >
-                  {isCreateRoute ? 'Create Class' : 'Edit Class'}
-                </button>
-              </div>
-            </div>
-          </form>
-        ) : null}
+    <section className={styles.formContainer}>
+      <div className={styles.formTitle}>
+        <h2 className={styles.title}>{isCreateRoute ? 'Create Class' : 'Edit Class'}</h2>
       </div>
+      <form className={styles.form}>
+        <div className={styles.inputContainer}>
+          <label className={styles.label}>Activity</label>
+          <select
+            value={classes.activity}
+            name="activity"
+            className={styles.select}
+            onChange={onChangeInput}
+          >
+            <option value="">Select an activity</option>
+            {activities.map((activity) => (
+              <option key={activity._id} value={activity._id}>
+                {activity.name}
+              </option>
+            ))}
+          </select>
+          {error && classes.activity === '' ? (
+            <span className={styles.error}>Field is required</span>
+          ) : null}
+        </div>
+        <div className={styles.inputContainer}>
+          <label className={styles.label}>Trainer</label>
+          <select
+            className={styles.select}
+            value={classes.trainer}
+            name="trainer"
+            onChange={onChangeInput}
+          >
+            <option value="">Select a trainer</option>
+            {trainers.map((trainer) => (
+              <option key={trainer._id} value={trainer._id}>
+                {trainer.firstName}
+              </option>
+            ))}
+          </select>
+          {error && classes.trainer === '' ? (
+            <span className={styles.error}>Field is required</span>
+          ) : null}
+        </div>
+        <div className={styles.inputContainer}>
+          <label className={styles.label}>Day</label>
+          <select className={styles.select} value={classes.day} name="day" onChange={onChangeInput}>
+            <option value="">Select a day</option>
+            <option value="Monday">Monday</option>
+            <option value="Tuesday">Tuesday</option>
+            <option value="Wednesday">Wednesday</option>
+            <option value="Thursday">Thursday</option>
+            <option value="Friday">Friday</option>
+            <option value="Saturday">Saturday</option>
+            <option value="Sunday">Sunday</option>
+          </select>
+          {error && classes.day === '' ? (
+            <span className={styles.error}>Field is required</span>
+          ) : null}
+        </div>
+        <div className={styles.inputContainer}>
+          <label className={styles.label}>Time</label>
+          <input
+            className={styles.input}
+            type="time"
+            value={classes.time}
+            name="time"
+            onChange={onChangeInput}
+          />
+          {(error && classes.time === '') || (error && classes.time === 0) ? (
+            <span className={styles.error}>Field is required</span>
+          ) : null}
+        </div>
+        <div className={styles.inputContainer}>
+          <label className={styles.label}>Capacity</label>
+          <input
+            className={styles.input}
+            type="number"
+            value={classes.capacity}
+            name="capacity"
+            onChange={onChangeInput}
+          />
+          {error && classes.capacity === '' ? (
+            <span className={styles.error}>Field is required</span>
+          ) : null}
+        </div>
+        <div className={styles.buttonContainer}>
+          <button
+            className={styles.button}
+            onClick={isCreateRoute ? onClickCreateClass : onClickEditClass}
+          >
+            {isCreateRoute ? 'Create Class' : 'Edit Class'}
+          </button>
+        </div>
+      </form>
       <Modal
         showModal={showModal}
         responseModal={responseModal}
         onClose={() => setShowModal(!showModal)}
-      ></Modal>
-    </>
+      />
+    </section>
   );
 }
 
