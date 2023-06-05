fb3b13e2ddce53c7149bc892e347671c5854ec18
Mauro Caffesse <mscaffesse@gmail.com>
2023-06-05 12:26:15 -0300

    MR-80: old components deleted and replaced by the shared components. Form modified with shared components and improved functions

--- src/Components/Trainers/Form/index.jsx
+++ src/Components/Trainers/Form/index.jsx
@@ -1,11 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import ResponseModal from '../Modals/ResponseModal';
+import { useParams, useHistory } from 'react-router-dom';
+import ResponseModal from '../../Shared/ResponseModal';
+import ConfirmModal from '../../Shared/ConfirmModal';
+import Button from '../../Shared/Button';
 import styles from './form.module.css';
+import { Input } from '../../Shared/Inputs';
 
 const Form = () => {
   const { id } = useParams();
+  const history = useHistory();
   const [showResponseModal, setShowResponseModal] = useState(false);
+  const [stateModal, setStateModal] = useState('success');
+  const [showConfirmModal, setShowConfirmModal] = useState(false);
   const [selectedTrainer, setSelectedTrainer] = useState(false);
   const [responseMessage, setResponseMessage] = useState('');
   const [trainer, setTrainer] = useState({
@@ -19,17 +25,25 @@ const Form = () => {
   });
 
   useEffect(() => {
-    if (id) {
-      fetch(`${process.env.REACT_APP_API_URL}/api/trainer/${id}`)
-        .then((response) => response.json())
-        .then((data) => {
+    const fetchData = async () => {
+      try {
+        if (id) {
+          const response = await fetch(`${process.env.REACT_APP_API_URL}/api/trainer/${id}`);
+          const data = await response.json();
           setSelectedTrainer(data.data);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    }
-  }, []);
+        }
+      } catch (error) {
+        setResponseMessage(`Error getting trainer: ${error.message}`);
+        setShowResponseModal(true);
+        setStateModal('fail');
+        setTimeout(() => {
+          setShowResponseModal(false);
+        }, 3000);
+      }
+    };
+
+    fetchData();
+  }, [id]);
 
   useEffect(() => {
     setTrainer({
@@ -50,13 +64,14 @@ const Form = () => {
     });
   };
 
+  const handleSubmit = (e) => {
+    setShowConfirmModal(false);
+    onSubmit(e);
+  };
+
   const onSubmit = (e) => {
     e.preventDefault();
-    if (id) {
-      updTrainer(selectedTrainer._id, trainer);
-    } else {
-      addTrainer(trainer);
-    }
+    id ? updTrainer(selectedTrainer._id, trainer) : addTrainer(trainer);
   };
 
   const addTrainer = async (trainer) => {
@@ -71,13 +86,23 @@ const Form = () => {
       if (response.ok) {
         setResponseMessage('Trainer added successfully');
         setShowResponseModal(true);
+        setStateModal('success');
+        setTimeout(() => {
+          setShowResponseModal(false);
+          history.push('/trainers');
+        }, 1500);
       } else {
         setResponseMessage('Failed to add trainer');
         setShowResponseModal(true);
+        setStateModal('fail');
+        setTimeout(() => {
+          setShowResponseModal(false);
+        }, 3000);
       }
     } catch (error) {
       setResponseMessage(`Error adding trainer: ${error.message}`);
       setShowResponseModal(true);
+      setStateModal('fail');
     }
   };
 
@@ -93,119 +118,150 @@ const Form = () => {
       if (response.ok) {
         setResponseMessage('Trainer updated successfully');
         setShowResponseModal(true);
+        setStateModal('success');
+        setTimeout(() => {
+          setShowResponseModal(false);
+          history.push('/trainers');
+        }, 1500);
       } else {
         setResponseMessage('Failed to update trainer');
         setShowResponseModal(true);
+        setStateModal('fail');
+        setTimeout(() => {
+          setShowResponseModal(false);
+        }, 3000);
       }
     } catch (error) {
       setResponseMessage(`Error updating trainer: ${error.message}`);
       setShowResponseModal(true);
+      setStateModal('fail');
     }
+  };
+
+  const handleConfirmModal = () => {
+    if (validateInputs()) {
+      setShowConfirmModal(true);
+    } else {
+      setResponseMessage('Please fill in all fields');
+      setShowResponseModal(true);
+      setStateModal('fail');
+      setTimeout(() => {
+        setShowResponseModal(false);
+      }, 3000);
+    }
+  };
+
+  const validateInputs = () => {
+    for (const key in trainer) {
+      if (trainer[key].trim() === '') {
+        return false;
+      }
+    }
+    return true;
   };
 
   return (
     <>
       <h2 className={styles.title}>{id ? 'Edit Trainer' : 'Add Trainer'}</h2>
-      <form onSubmit={onSubmit} className={styles.container}>
+      <form className={styles.container}>
         <div className={styles.flex}>
-          <label htmlFor="firstName" className={styles.label}>
-            First Name
-          </label>
-          <input
+          <Input
+            labelText="First Name"
             name="firstName"
             type="text"
-            id="firstName"
             value={trainer.firstName}
-            onChange={onChangeInput}
+            change={onChangeInput}
             className={styles.input}
           />
         </div>
         <div className={styles.flex}>
-          <label htmlFor="lastName" className={styles.label}>
-            Last Name
-          </label>
-          <input
+          <Input
+            labelText="Last Name"
             name="lastName"
             type="text"
-            id="lastName"
             value={trainer.lastName}
-            onChange={onChangeInput}
+            change={onChangeInput}
             className={styles.input}
           />
         </div>
         <div className={styles.flex}>
-          <label htmlFor="dni" className={styles.label}>
-            ID
-          </label>
-          <input
+          <Input
+            labelText="ID"
             name="dni"
             type="text"
-            id="dni"
             value={trainer.dni}
-            onChange={onChangeInput}
+            change={onChangeInput}
             className={styles.input}
           />
         </div>
         <div className={styles.flex}>
-          <label htmlFor="phone" className={styles.label}>
-            Phone Number
-          </label>
-          <input
+          <Input
+            labelText="Phone"
             name="phone"
             type="text"
-            id="phone"
             value={trainer.phone}
-            onChange={onChangeInput}
+            change={onChangeInput}
             className={styles.input}
           />
         </div>
         <div className={styles.flex}>
-          <label htmlFor="email" className={styles.label}>
-            Email
-          </label>
-          <input
+          <Input
+            labelText="Email"
             name="email"
             type="email"
-            id="email"
             value={trainer.email}
-            onChange={onChangeInput}
+            change={onChangeInput}
             className={styles.input}
           />
         </div>
         <div className={styles.flex}>
-          <label htmlFor="password" className={styles.label}>
-            Password
-          </label>
-          <input
+          <Input
+            labelText="Password"
             name="password"
             type="password"
-            id="password"
             value={trainer.password}
-            onChange={onChangeInput}
+            change={onChangeInput}
             className={styles.input}
           />
         </div>
         <div className={styles.flex}>
-          <label htmlFor="salary" className={styles.label}>
-            Salary
-          </label>
-          <input
+          <Input
+            labelText="Salary"
             name="salary"
             type="text"
-            id="salary"
             value={trainer.salary}
-            onChange={onChangeInput}
+            change={onChangeInput}
             className={styles.input}
           />
         </div>
-        <div>
-          <button type="submit" className={styles.save}>
-            {id ? 'Update' : 'Submit'}
-          </button>
-        </div>
       </form>
+      <div className={styles.btnContainer}>
+        <Button
+          text={id ? 'Update' : 'Submit'}
+          classNameButton="addButton"
+          action={handleConfirmModal}
+        >
+          {id ? 'Update' : 'Submit'}
+        </Button>
+      </div>
+      {showConfirmModal && (
+        <ConfirmModal
+          title={id ? 'Edit Trainer' : 'Add Trainer'}
+          handler={() => setShowConfirmModal(false)}
+          onAction={handleSubmit}
+          reason={'submit'}
+        >
+          {id
+            ? 'Are you sure you want to edit this trainer?'
+            : 'Are you sure you want to add this trainer?'}
+        </ConfirmModal>
+      )}
       {showResponseModal && (
-        <ResponseModal text={responseMessage} onClose={() => setShowResponseModal(false)} />
+        <ResponseModal
+          message={responseMessage}
+          state={stateModal}
+          handler={() => setShowResponseModal(false)}
+        />
       )}
     </>
   );
