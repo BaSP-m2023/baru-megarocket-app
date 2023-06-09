660ffeee453af7549d179f33d43ca6d6b6b1f275
francoax <francoa.duarte2001@gmail.com>
2023-06-09 12:20:45 -0300

    MR-96: dependencies installed and index configured.

--- src/index.js
+++ src/index.js
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom';
 import Layout from './Components/Layout';
 import './index.css';
 
+import { Provider } from 'react-redux';
+import { store } from './Redux/store';
+
 ReactDOM.render(
-  <React.StrictMode>
-    <Layout />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <Layout />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
