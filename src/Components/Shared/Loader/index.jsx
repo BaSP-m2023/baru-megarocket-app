d01f6fed0c8d6bd1c957cf8e7e8fc78a3553bddf
jramire5 <jjnicoramirez@gmail.com>
2023-06-09 14:39:19 -0300

    MR-99: Added default to Loader function to directly export it

--- src/Components/Shared/Loader/index.jsx
+++ src/Components/Shared/Loader/index.jsx
@@ -1,6 +1,6 @@
 import styles from './loader.module.css';
 import React from 'react';
 
-export function Loader() {
+export default function Loader() {
   return <span className={styles.loader}></span>;
 }
