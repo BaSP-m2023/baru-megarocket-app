bdf6a529f1b68cfd13ba224289e8b36f8cc06b72
Ignacio <igna.galceran@gmail.com>
2023-06-10 16:39:18 -0300

    MR-106: GET class with redux

--- src/Redux/rootReducer.js
+++ src/Redux/rootReducer.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
 
 // Import reducers below
+import classReducer from './Classes/reducers';
 
-const rootReducers = combineReducers({});
+const rootReducers = combineReducers({
+  classes: classReducer
+});
 
 export default rootReducers;
