4ebb9cc5f6962e95089e15378c853075074df5bf
Mauro Caffesse <mscaffesse@gmail.com>
2023-06-11 14:13:29 -0300

    MR-100: more constants and actions added due to post, put and delete methods, and modals

--- src/Redux/Trainers/actions.js
+++ src/Redux/Trainers/actions.js
@@ -1,4 +1,13 @@
-import { GET_TRAINERS_PENDING, GET_TRAINERS_SUCCESS, GET_TRAINERS_ERROR } from './constants';
+import {
+  GET_TRAINERS_PENDING,
+  GET_TRAINERS_SUCCESS,
+  GET_TRAINERS_ERROR,
+  ADD_TRAINER_PENDING,
+  ADD_TRAINER_SUCCESS,
+  ADD_TRAINER_ERROR,
+  SHOW_RESPONSE_MODAL,
+  HIDE_RESPONSE_MODAL
+} from './constants';
 
 export const getTrainersPending = () => ({
   type: GET_TRAINERS_PENDING
@@ -13,3 +22,34 @@ export const getTrainersError = (error) => ({
   type: GET_TRAINERS_ERROR,
   payload: error
 });
+
+export const addTrainerPending = (trainer) => ({
+  type: ADD_TRAINER_PENDING,
+  payload: trainer
+});
+
+export const addTrainerSuccess = (trainer) => ({
+  type: ADD_TRAINER_SUCCESS,
+  payload: trainer
+});
+
+export const addTrainerError = (error) => ({
+  type: ADD_TRAINER_ERROR,
+  payload: error
+});
+
+export const showResponseModal = (message, state) => {
+  return {
+    type: SHOW_RESPONSE_MODAL,
+    payload: {
+      message,
+      state
+    }
+  };
+};
+
+export const hideResponseModal = () => {
+  return {
+    type: HIDE_RESPONSE_MODAL
+  };
+};
