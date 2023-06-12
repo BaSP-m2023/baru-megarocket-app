9a843c7effc1d3b543ce311b4df030648690770e
Alejandro <victoriaecalle@gmail.com>
2023-06-12 17:12:54 -0300

    MR-98: Added a timer to make ResponseModal dissapear after 4 seconds

--- src/Components/Shared/ResponseModal/index.jsx
+++ src/Components/Shared/ResponseModal/index.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import styles from './modalResponse.module.css';
+import { useState, useEffect } from 'react';
 
 const ResponseModal = ({ handler, state, message }) => {
   const responseState = {
     success: 'toastSuccess',
     fail: 'toastFail'
   };
+  const [visible, setVisible] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setVisible(false);
+    }, 4000);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
+  if (!visible) {
+    return null;
+  }
+
   return ReactDOM.createPortal(
     <div className={`${styles.toast} ${styles[responseState[state]]} `}>
       <div className={styles.toastContent}>
