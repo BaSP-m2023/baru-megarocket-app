2a84424d3050139cbbeb2eff135cc2da4aa8a263
RodrigoF <r.fontan12@gmail.com>
2023-06-14 14:17:29 -0300

    MR-116: Fix shared button trigger

--- src/Components/Shared/Button/index.jsx
+++ src/Components/Shared/Button/index.jsx
@@ -3,11 +3,16 @@ import styles from './button.module.css';
 
 function Button({ action, text, img, classNameButton }) {
   return (
-    <div onClick={action ? action : undefined}>
+    <div>
       {img ? (
         <img className={styles.icon} src={img} alt={text} />
       ) : (
-        <button className={`${styles.button} ${styles[classNameButton]}`}>{text}</button>
+        <button
+          className={`${styles.button} ${styles[classNameButton]}`}
+          onClick={action ? action : undefined}
+        >
+          {text}
+        </button>
       )}
     </div>
   );
