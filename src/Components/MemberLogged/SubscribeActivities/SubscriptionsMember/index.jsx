323609b79e5320fe27d21e0da898df53cc08f800
Walter Ariel Pavicich <walterarielpavicich@gmail.com>
2023-06-18 19:08:17 -0300

    MR-139: delete activity button added

--- src/Components/MemberLogged/SubscribeActivities/SubscriptionsMember/index.jsx
+++ src/Components/MemberLogged/SubscribeActivities/SubscriptionsMember/index.jsx
@@ -1,15 +1,22 @@
 import { getActivities } from 'Redux/Activities/thunks';
 import { getSubscriptions } from 'Redux/Subscriptions/thunks';
+import { deleteSubscription } from 'Redux/Subscriptions/thunks';
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Button from '../../../Shared/Button';
 import styles from './subscriptionMember.module.css';
+import ConfirmModal from '../../../Shared/ConfirmModal';
+import ResponseModal from '../../../Shared/ResponseModal';
+import { handleDisplayToast } from '../../../../Redux/Shared/ResponseToast/actions';
 
 const SubscriptionsMember = () => {
   const subscriptions = useSelector((state) => state.subscriptions.data);
   const activities = useSelector((state) => state.activities.list);
   const dispatch = useDispatch();
   const [subscription, setSubscription] = useState([]);
+  const [showConfirmModal, setShowConfirmModal] = useState(false);
+  const { show, message, state } = useSelector((state) => state.toast);
+  const [idToDelete, setIdToDelete] = useState('');
 
   useEffect(() => {
     getSubscriptions(dispatch);
@@ -19,7 +26,6 @@ const SubscriptionsMember = () => {
   useEffect(() => {
     getActivitiesName().then((data) => {
       setSubscription(data);
-      console.log(data);
     });
   }, [subscriptions, activities]);
 
@@ -44,10 +50,20 @@ const SubscriptionsMember = () => {
     return arraySubs;
   };
 
+  const handleDeleteActivity = () => {
+    dispatch(deleteSubscription(idToDelete));
+    setShowConfirmModal(false);
+  };
+
+  const handleDeleteButton = (id) => {
+    setShowConfirmModal(true);
+    setIdToDelete(id);
+  };
+
   return (
     <>
       <table className={styles.table}>
-        <thead>
+        <thead className={styles.thead}>
           <tr>
             <th className={styles.th}>Activity</th>
             <th className={styles.th}>Day</th>
@@ -63,17 +79,36 @@ const SubscriptionsMember = () => {
                 <td className={styles.td}>{item.day}</td>
                 <td className={styles.td}>{item.time}</td>
                 <td className={styles.button}>
-                  <Button
-                    text="Delete Button"
-                    img={`${process.env.PUBLIC_URL}/assets/images/delete-icon.png`}
-                    classNameButton="icon"
-                  />
+                  <div className={styles.buttonContainer}>
+                    <Button
+                      text="Unsubscribe"
+                      classNameButton="deleteButton"
+                      action={() => handleDeleteButton(item.subId)}
+                    />
+                  </div>
                 </td>
               </tr>
             );
           })}
         </tbody>
       </table>
+      {showConfirmModal && (
+        <ConfirmModal
+          handler={() => setShowConfirmModal(false)}
+          title="Unsubscribe Activity"
+          reason="delete"
+          onAction={() => handleDeleteActivity()}
+        >
+          Are you sure you want to unsubscribe activity?
+        </ConfirmModal>
+      )}
+      {show && (
+        <ResponseModal
+          handler={() => dispatch(handleDisplayToast(false))}
+          state={state}
+          message={message}
+        />
+      )}
     </>
   );
 };
