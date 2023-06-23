756b662bf2294572eb7f92b8cf05a9b940983863
santimenendez98 <santi.mm11@gmail.com>
2023-06-23 12:28:49 -0300

    MR-144: firebase setup

---
+++ src/Components/helper/firebase/index.js
@@ -0,0 +1,26 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_SENDER_ID,
+  appId: process.env.REACT_APP_ID
+};
+
+export const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+export const tokenListener = () => {
+  firebaseApp.auth().onIdTokenChanged(async (user) => {
+    if (user) {
+      const token = await user.getIdToken();
+      const {
+        classes: { role }
+      } = await user.getIdTokenResult();
+      sessionStorage.setItem('role', role);
+      sessionStorage.setItem('token', token);
+    }
+  });
+};
