190c576076cc00c25789009609b5618d90bbd26a
Alejandro <victoriaecalle@gmail.com>
2023-06-23 20:23:35 -0300

    MR-152: Added redux auth and firebase

---
+++ src/Firebase/index.js
@@ -0,0 +1,28 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+};
+
+// Initialize Firebase
+export const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+export const tokenListener = () => {
+  firebase.auth().onIdTokenChanged(async (user) => {
+    if (user) {
+      const token = await user.getIdToken();
+      const {
+        claims: { role }
+      } = await user.getIdTokenResult();
+      sessionStorage.setItem('role', role);
+      sessionStorage.setItem('token', token);
+    }
+  });
+};
---
+++ src/Redux/Auth/thunks.js
@@ -0,0 +1,139 @@
+import {
+  loginError,
+  loginPending,
+  loginSuccess,
+  signUpError,
+  signUpPending,
+  signUpSuccess,
+  getAuthenticationPending,
+  getAuthenticationSuccess,
+  getAuthenticationError,
+  logoutSuccess,
+  logoutError,
+  logoutPending
+} from './actions';
+
+import { firebaseApp } from '../../helper/firebase';
+
+export const login = (credentials) => {
+  return async (dispatch) => {
+    dispatch(loginPending());
+    try {
+      const firebaseResponse = await firebaseApp.auth()
+        .signInWithEmailAndPassword(credentials.email, credentials.password)
+      const token = await firebaseResponse.user.getIdToken();
+      const {
+        claims: { role }
+      } = await firebaseResponse.user.getIdTokenResult();
+      return dispatch(loginSuccess({ role, token }))
+    } catch (error) {
+      return dispatch(loginError(error.toString()));
+    }
+  };
+};
+
+export const getAuth = (token) => {
+  return async (dispatch) => {
+    dispatch(getAuthenticationPending());
+    try {
+      const response = fetch(`${process.env.REACT_APP_API_URL}/api/auth/`, { headers: { token } });
+      const res = await response.json();
+      dispatch(getAuthenticationSuccess(res.data));
+      return res.data;
+    } catch (error) {
+      dispatch(getAuthenticationError(error.toString()));
+    }
+  }
+}
+
+export const logOut = () => {
+  return async (dispatch) => {
+    dispatch(logoutPending())
+    try {
+      await firebaseApp.auth().signOut();
+      dispatch(logoutSuccess());
+      sessionStorage.removeItem('token', '');
+      sessionStorage.removeItem('role', '')
+      return { error: false, message: 'Log Out Successfully' };
+    } catch (error) {
+      console.error(error);
+      dispatch(logoutError(error))
+      return {
+        error: true,
+        message: error
+      };
+    }
+  }
+}
+
+export const signUpMember = (data) => {
+  return async (dispatch) => {
+    dispatch(signUpPending());
+    try {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/members`, {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      })
+      const res = await response.json();
+      if (response.error) {
+        throw new Error(response.message);
+      }
+      await dispatch(signUpSuccess(data));
+      return res;
+    } catch (error) {
+      dispatch(signUpError(error.toString()));
+    }
+  }
+}
+
+export const signUpAdmin = (data) => {
+  return async (dispatch) => {
+    dispatch(signUpPending());
+    try {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/admins`, {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      })
+      const res = await response.json();
+      if (response.error) {
+        throw new Error(response.message);
+      }
+      await dispatch(signUpSuccess(data));
+      return res;
+    } catch (error) {
+      dispatch(signUpError(error.toString()));
+    }
+  }
+}
+
+export const signUpTrainer = (data) => {
+  return async (dispatch) => {
+    dispatch(signUpPending());
+    try {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/trainer`, {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      })
+      const res = await response.json();
+      if (response.error) {
+        throw new Error(response.message);
+      }
+      await dispatch(signUpSuccess(data));
+      return res;
+    } catch (error) {
+      dispatch(signUpError(error.toString()));
+    }
+  }
+}
