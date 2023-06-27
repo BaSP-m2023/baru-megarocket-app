ac96021e1b102e7886a2303014930758a73ae3ca
gabiholl <gabrielahollmann@gmail.com>
2023-06-27 20:14:11 -0300

    MR-173:subscription and logout PO, final e2e members

--- test/pageobjects/Member/ActivitiesPage.js
+++ test/pageobjects/Member/ActivitiesPage.js
@@ -3,12 +3,47 @@ class Activities {
     return $("[data-testid='routes-list']>a:nth-child(2)");
   }
 
+  //activities home
   get ActivitiesBox() {
     return $("[data-testid='activities-cards-container']> div:nth-child(1)");
   }
 
-  get ActivitiesBas() {
+  get ActivitiesBasquet() {
     return $("[data-testid='activities-cards-container']> div:nth-child(2)");
+  }
+
+  //activities subscription
+  get ActivitiesSubs() {
+    return $("[data-testid='activities-cards-container']");
+  }
+
+  get ActivitiesSubsBasquet() {
+    return $("[data-testid='activity-subscription-container']");
+  }
+
+  get ActivitiesSelection() {
+    return $(
+      "[data-testid='activity-subscription-container']> form > select > option:nth-child(1)"
+    );
+  }
+
+  get ActivitiesSubsSelect() {
+    return $(
+      "[data-testid='activity-subscription-container']> form > select > option:nth-child(2)"
+    );
+  }
+
+  //buttons
+  get submitBtn() {
+    return $("[data-testid='activity-subscription-container']> form > div > div > button");
+  }
+
+  get cancelBtn() {
+    return $("[data-testid='activity-subscription-container']> form > div > a > div > button");
+  }
+
+  get submitBtnBasmodal() {
+    return $("[data-testid='confirm-modal-buttons']> div:nth-child(2) > button");
   }
 }
 
--- test/specs/Member/e2eMember.js
+++ test/specs/Member/e2eMember.js
@@ -1,6 +1,7 @@
 const LoginPage = require('../../pageobjects/LoginPage');
 const ActivitiesPage = require('../../pageobjects/Member/ActivitiesPage');
-
+const SubscriptionPage = require('../../pageobjects/Member/SubscriptionPage');
+const LogoutPage = require('../../pageobjects/Member/LogoutPage');
 describe('Verify login with user with valid credentials', () => {
   beforeAll('Open browser', () => {
     browser.url('https://baru-megarocket-app.vercel.app/');
@@ -21,11 +22,35 @@ describe('Verify login with user with valid credentials', () => {
     await expect(browser).toHaveUrl('https://baru-megarocket-app.vercel.app/');
     await expect(LoginPage.homePage).toBeDisplayed();
   });
-  it('user should get into activities', async () => {
+  it('user should click boxer activity', async () => {
     await ActivitiesPage.ActivitiesNavBar.click();
+    await expect(ActivitiesPage.ActivitiesSubs).toBeDisplayed();
+    await ActivitiesPage.ActivitiesBox.click();
+    await ActivitiesPage.cancelBtn.click();
+  });
+  it('user should subscribe to basquet activity', async () => {
+    await ActivitiesPage.ActivitiesBasquet.click();
+    await expect(ActivitiesPage.ActivitiesSubsBasquet).toBeDisplayed();
+    await ActivitiesPage.ActivitiesSelection.click();
+    await ActivitiesPage.ActivitiesSubsSelect.click();
+    await ActivitiesPage.submitBtn.click();
+    await ActivitiesPage.submitBtnBasmodal.click();
     await expect(browser).toHaveUrl(
       'https://baru-megarocket-app.vercel.app/user/members/subscribe-class'
     );
-    await expect(LoginPage.homePage).toBeDisplayed();
+  });
+  it('user should unsubscribe from basquet activity', async () => {
+    await SubscriptionPage.SubscriptionNavBar.click();
+    await expect(SubscriptionPage.Subscriptionlist).toBeDisplayed();
+    await SubscriptionPage.UnSubscriptionListBtn.click();
+    await expect(SubscriptionPage.SubscriptionModal).toBeDisplayed();
+    await SubscriptionPage.deleteBtnmodal.click();
+    await expect(browser).toHaveUrl(
+      'https://baru-megarocket-app.vercel.app/user/members/subscriptions'
+    );
+  });
+  it('user should be able to logout', async () => {
+    await LogoutPage.LogOutBtn.click();
+    await expect(browser).toHaveUrl('https://baru-megarocket-app.vercel.app/');
   });
 });
