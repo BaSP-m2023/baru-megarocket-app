0b27585cb630ad76fa9b3db03645815a9855889c
IgnacioGalceran <igna.galceran@gmail.com>
2023-06-27 21:32:13 -0300

    MR-182: Form fix of members

--- src/Components/Members/Form/index.jsx
+++ src/Components/Members/Form/index.jsx
@@ -7,21 +7,20 @@ import Button from 'Components/Shared/Button';
 import { Input } from 'Components/Shared/Inputs';
 import { addMember, updateMember } from 'Redux/Members/thunks';
 import { useDispatch, useSelector } from 'react-redux';
-import { handleDisplayToast, setContentToast } from 'Redux/Shared/ResponseToast/actions';
+import { handleDisplayToast } from 'Redux/Shared/ResponseToast/actions';
+import { getMembers } from 'Redux/Members/thunks';
 import { useForm } from 'react-hook-form';
 import memberSchema from 'Validations/member';
+import memberUpdate from 'Validations/memberUpdate';
 import { joiResolver } from '@hookform/resolvers/joi';
 
 const MemberForm = ({ match }) => {
   const [modalMessageOpen, setModalMessageOpen] = useState(false);
   const history = useHistory();
   let memberId = match.params.id;
-  const members = useSelector((state) => state.members.data);
-
   const dispatch = useDispatch();
   const redirect = useSelector((state) => state.members.redirect);
   const { show, message, state } = useSelector((state) => state.toast);
-  const memberToEdit = members.find((member) => member._id === memberId);
 
   const {
     register,
@@ -29,45 +28,52 @@ const MemberForm = ({ match }) => {
     reset,
     setValue,
     formState: { errors }
-  } = useForm({
-    mode: 'onChange',
-    resolver: joiResolver(memberSchema),
-    defaultValues: {
-      name: '',
-      lastName: '',
-      dni: '',
-      phone: '',
-      email: '',
-      city: '',
-      dob: '',
-      zip: '',
-      isActive: '',
-      membership: 'default',
-      password: ''
-    }
-  });
+  } = !memberId
+    ? useForm({
+        mode: 'onChange',
+        resolver: joiResolver(memberSchema),
+        defaultValues: {
+          name: '',
+          lastName: '',
+          dni: '',
+          phone: '',
+          email: '',
+          city: '',
+          dob: '',
+          zip: '',
+          isActive: '',
+          membership: 'default',
+          password: ''
+        }
+      })
+    : useForm({
+        mode: 'onChange',
+        resolver: joiResolver(memberUpdate),
+        defaultValues: {
+          name: '',
+          lastName: '',
+          dni: '',
+          phone: '',
+          city: '',
+          dob: '',
+          zip: '',
+          isActive: '',
+          membership: 'default'
+        }
+      });
 
   useEffect(() => {
-    const getMember = async () => {
-      try {
-        setValue('name', memberToEdit.name);
-        setValue('lastName', memberToEdit.lastName);
-        setValue('dni', memberToEdit.dni);
-        setValue('phone', memberToEdit.phone);
-        setValue('email', memberToEdit.email);
-        setValue('city', memberToEdit.city);
-        setValue('dob', memberToEdit.dob.slice(0, 10));
-        setValue('zip', memberToEdit.zip);
-        setValue('isActive', memberToEdit.isActive);
-        setValue('membership', memberToEdit.membership);
-        setValue('password', memberToEdit.password);
-      } catch (error) {
-        dispatch(setContentToast({ message: error.message, state: 'fail' }));
-        dispatch(handleDisplayToast(true));
-      }
-    };
     if (memberId) {
-      getMember(memberId);
+      getMembers(dispatch).then((data) => {
+        const member = data.find((item) => item._id === memberId);
+        // eslint-disable-next-line no-unused-vars
+        const { _id, firebaseUid, email, __v, dob, ...resMember } = member;
+        Object.entries(resMember).every(([key, value]) => {
+          setValue(key, value);
+          return true;
+        });
+        setValue('dob', dob.slice(0, 10));
+      });
     }
   }, []);
 
@@ -91,6 +97,30 @@ const MemberForm = ({ match }) => {
     setModalMessageOpen(true);
   };
 
+  const formCreate = [
+    { labelText: 'First Name', name: 'name', type: 'text' },
+    { labelText: 'Last Name', name: 'lastName', type: 'text' },
+    { labelText: 'ID', name: 'dni', type: 'number' },
+    { labelText: 'Phone', name: 'phone', type: 'text' },
+    { labelText: 'City', name: 'city', type: 'text' },
+    { labelText: 'Zip', name: 'zip', type: 'number' },
+    { labelText: 'Email', name: 'email', type: 'email' },
+    { labelText: 'Password', name: 'password', type: 'password' },
+    { labelText: 'Date of birth', name: 'dob', type: 'date' },
+    { labelText: 'Is member active?', name: 'isActive', type: 'checkbox' }
+  ];
+
+  const formEdit = [
+    { labelText: 'First Name', name: 'name', type: 'text' },
+    { labelText: 'Last Name', name: 'lastName', type: 'text' },
+    { labelText: 'ID', name: 'dni', type: 'number' },
+    { labelText: 'Phone', name: 'phone', type: 'text' },
+    { labelText: 'City', name: 'city', type: 'text' },
+    { labelText: 'Zip', name: 'zip', type: 'number' },
+    { labelText: 'Date of birth', name: 'dob', type: 'date' },
+    { labelText: 'Is member active?', name: 'isActive', type: 'checkbox' }
+  ];
+
   return (
     <div className={styles.form}>
       <div className={styles.content}>
@@ -101,104 +131,36 @@ const MemberForm = ({ match }) => {
           </span>
         </div>
         <form className={styles.body} data-testid="members-form-container">
-          <div className={styles.label_container}>
-            <Input
-              labelText="Name"
-              type="text"
-              name="name"
-              error={errors.name?.message}
-              register={register}
-            />
-          </div>
-          <div className={styles.label_container}>
-            <Input
-              labelText="Last Name"
-              type="text"
-              name="lastName"
-              error={errors.lastName?.message}
-              register={register}
-            />
-          </div>
-          <div className={styles.label_container}>
-            <Input
-              labelText="DNI"
-              type="number"
-              name="dni"
-              error={errors.dni?.message}
-              register={register}
-            />
-          </div>
-          <div className={styles.label_container}>
-            <Input
-              labelText="Phone"
-              type="text"
-              name="phone"
-              error={errors.phone?.message}
-              register={register}
-            />
-          </div>
-          <div className={styles.label_container}>
-            <Input
-              labelText="Email"
-              type="string"
-              name="email"
-              error={errors.email?.message}
-              register={register}
-            />
-          </div>
-          <div className={styles.label_container}>
-            <Input
-              labelText="City"
-              type="text"
-              name="city"
-              error={errors.city?.message}
-              register={register}
-            />
-          </div>
-          <div className={styles.label_container}>
-            <Input
-              labelText="Date of birth"
-              type="date"
-              name="dob"
-              error={errors.dob?.message}
-              register={register}
-            />
-          </div>
-          <div className={styles.label_container}>
-            <Input
-              labelText="Zip code"
-              type="number"
-              name="zip"
-              error={errors.zip?.message}
-              register={register}
-            />
-          </div>
-          <div className={styles.label_container}>
-            <label className={styles.label}>Membership</label>
-            <select className={styles.input} name="membership" {...register('membership')}>
-              <option value="default">Choose your membership</option>
-              <option value="classic">Classic</option>
-              <option value="only_classes">Only Classes</option>
-              <option value="black">Black</option>
-            </select>
-          </div>
-          <div className={styles.label_container}>
-            <Input
-              labelText="Password"
-              type="password"
-              name="password"
-              error={errors.password?.message}
-              register={register}
-            />
-          </div>
-          <div className={`${styles.label_container} ${styles.checkbox}`}>
-            <Input
-              labelText="Is member active?"
-              type="checkbox"
-              name="isActive"
-              register={register}
-            />
-          </div>
+          {!memberId
+            ? formCreate.map((field) => (
+                <div className={styles.flex} key={field.name}>
+                  <Input
+                    labelText={field.labelText}
+                    name={field.name}
+                    type={field.type}
+                    register={register}
+                    error={errors[field.name]?.message}
+                  />
+                </div>
+              ))
+            : formEdit.map((field) => (
+                <div className={styles.flex} key={field.name}>
+                  <Input
+                    labelText={field.labelText}
+                    name={field.name}
+                    type={field.type}
+                    register={register}
+                    error={errors[field.name]?.message}
+                  />
+                </div>
+              ))}
+          <label className={styles.label}>Membership</label>
+          <select className={styles.input} name="membership" {...register('membership')}>
+            <option value="default">Choose your membership</option>
+            <option value="classic">Classic</option>
+            <option value="only_classes">Only Classes</option>
+            <option value="black">Black</option>
+          </select>
           <div className={styles.reset_button}>
             <Button action={reset} text="Reset" classNameButton="deleteButton" />
           </div>
