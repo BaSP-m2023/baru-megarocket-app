e171c7422f6669f2218783a87567fefed9e11631
francoax <francoa.duarte2001@gmail.com>
2023-07-02 23:50:57 -0300

    MR-189: add new validation on privateRoute and removed unused parameter from function.

--- src/Routes/privateRoute.jsx
+++ src/Routes/privateRoute.jsx
@@ -12,7 +12,12 @@ const PrivateRoute = ({ component: RouteComponent, ...rest }) => {
         if (token && role === rest.role) {
           return <RouteComponent {...routeProps} />;
         }
-        return <Redirect to={'/auth/login'} />;
+        if (token && role) {
+          return <Redirect to={`/user/${role.toLowerCase()}/`} />;
+        }
+        if (!token && !role) {
+          return <Redirect to="/auth/login" />;
+        }
       }}
     />
   );
