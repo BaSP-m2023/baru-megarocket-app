874194a4051c2314ecde8352c5c7267354f4b25d
francoax <francoa.duarte2001@gmail.com>
2023-07-17 13:43:00 -0300

    MR-246: header and navbar styling reformated.

--- src/Components/Layout/Header/index.jsx
+++ src/Components/Layout/Header/index.jsx
@@ -3,7 +3,7 @@ import { Link, useHistory } from 'react-router-dom';
 import styles from './header.module.css';
 import React, { useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faToggleOff, faToggleOn } from '@fortawesome/free-solid-svg-icons';
+import { faToggleOff, faToggleOn, faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 
 import { logOut } from 'Redux/Auth/thunks';
 import { handleDisplayToast, setContentToast } from 'Redux/Shared/ResponseToast/actions';
@@ -37,35 +37,46 @@ function Header(props) {
 
   return (
     <header className={styles.container}>
+      <Link to={props.routes[0].path} className={styles.link}>
+        <div data-testid="logo-container" className={styles.logoContainer}>
+          <img
+            src={`${process.env.PUBLIC_URL}/assets/images/logo3.png`}
+            alt="Rocket"
+            className={styles.logo}
+          />
+          <p className={styles.mega}>Mega</p>
+          <p className={styles.rocket}>Rocket</p>
+        </div>
+      </Link>
       <NavBar routes={props.routes} />
       <div className={styles.container2}>
         {role && userLogged && (
           <div className={styles.userContainer}>
-            {history.location.pathname.endsWith('/home') && (
-              <div className={styles.toggleContainer}>
-                {dark ? (
-                  <FontAwesomeIcon
-                    icon={faToggleOn}
-                    onClick={() => {
-                      dispatch(setDarkMode(false));
-                      localStorage.setItem('darkMode', JSON.stringify(false));
-                    }}
-                    className={styles.toggle}
-                    size="2xl"
-                  />
-                ) : (
-                  <FontAwesomeIcon
-                    icon={faToggleOff}
-                    onClick={() => {
-                      dispatch(setDarkMode(true));
-                      localStorage.setItem('darkMode', JSON.stringify(true));
-                    }}
-                    className={styles.toggle}
-                    size="2xl"
-                  />
-                )}
-              </div>
-            )}
+            <div className={styles.toggleContainer}>
+              <FontAwesomeIcon icon={faSun} className={`${styles.iconMode} ${styles.sun}}`} />
+              {dark ? (
+                <FontAwesomeIcon
+                  icon={faToggleOn}
+                  onClick={() => {
+                    dispatch(setDarkMode(false));
+                    localStorage.setItem('darkMode', JSON.stringify(false));
+                  }}
+                  className={styles.toggle}
+                  size="2xl"
+                />
+              ) : (
+                <FontAwesomeIcon
+                  icon={faToggleOff}
+                  onClick={() => {
+                    dispatch(setDarkMode(true));
+                    localStorage.setItem('darkMode', JSON.stringify(true));
+                  }}
+                  className={styles.toggle}
+                  size="2xl"
+                />
+              )}
+              <FontAwesomeIcon icon={faMoon} className={`${styles.iconMode} ${styles.moon}}`} />
+            </div>
             <Link
               className={styles.profileLink}
               to={
@@ -103,7 +114,6 @@ function Header(props) {
                     <path d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z"></path>
                   </svg>
                 </div>
-
                 <div className={styles.text}>Logout</div>
               </button>
             </div>
